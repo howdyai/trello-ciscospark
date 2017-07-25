abf33743912f23f476715a7c5387753f2190d571
jonchurch <me@jonchurch.com>
2017-07-24 22:57:54 -0400

    search user cards with search command

--- skills/trelloWorld.js
+++ skills/trelloWorld.js
@@ -4,8 +4,9 @@ const t = new Trello(process.env.T_KEY, process.env.T_TOKEN)
 
 module.exports = function(controller) {
 
-	controller.hears('(.*)', 'direct_message,direct_mention', function(bot, message){
-		t.get("/1/members/me/boards", {filter: 'organization', organization: true, fields: 'name,id'}, function(err, data) {
+	// list all user boards
+	controller.hears('^boards$', 'direct_message,direct_mention', function(bot, message){
+		t.get("/1/members/me/boards", { organization: true, fields: 'name,id'}, function(err, data) {
 			if (err) {
 				console.log('err:', err)
 			} else {
@@ -16,6 +17,48 @@ module.exports = function(controller) {
 				boardList = boardList.join('')
 				bot.reply(message, "**Pick a Board, respond with its number:**" + boardList)
 			}
+		})
+	})
+	
+	// list all user orgs
+	controller.hears('^orgs$', 'direct_message', function(bot, message) {
+
+		t.get('1/members/me/organizations', {fields: 'displayName,id'}, (err, data) => {
+		if (err) {
+			console.log('err:', err)
+			bot.reply(message, 'Something has gone wrong')
+		} else {
+			console.log({data})
+			let orgList = data.map((el, i) => `\n\n**${i}:** ${el.displayName}`)
+					.join('')
+			bot.reply(message, "**Organizations you belong to:**" + orgList)
+		}
+		})
+	})
+	
+	// search cards
+	controller.hears('^search(.*)', 'direct_message,direct_mention)', function(bot, message) {
+		const query = message.match[1].trim()
+		console.log({query})
+
+		t.get('1/search', {
+			query: query,
+			modelTypes: 'cards',
+			card_fields: 'name,desc,due,subscribed',
+			cardList: true,
+			partial: true
+		},
+			(err, data) => {
+		if (err) {
+			console.log('err:', err)
+			bot.reply(message, 'Something has gone wrong')
+		} else {
+			console.log({data})
+			console.log(data.cards[0])
+			let searchResults = data.cards.map((el, i) => `\n\n**${i}:** ${el.name}\n\n${el.desc}`)
+					.join('')
+			bot.reply(message, "**Search Results:**" + searchResults)
+		}
 		})
 	})
 }
