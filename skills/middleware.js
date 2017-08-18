3dae404e376d4889761819f37c6b65ddadb25cbd
jonchurch <me@jonchurch.com>
2017-08-18 17:58:38 -0400

    break user middleware into three smaller

--- skills/middleware.js
+++ skills/middleware.js
@@ -1,74 +1,37 @@
 
 module.exports = (controller) => {
 
-
-     controller.middleware.receive.use((bot, message, next) => {
-		 console.log('=======RECEIVE MIDDLEWARE FIRED')
-		controller.storage.users.get(message.user, (err, user) => {
-			console.log({message})
-
-			if (err) {// there is an error if the user isnt in the db
-				// okay it looks like I'm not getting out of here until at least I setup a non-found user and pass it on message to next
-				console.log('Err getting user from storage: ', err)
-			} if (! user) {
-
-				setupUser(message, (err, user) => {
-					console.log('=====NEW USER:\n', user)
-					if (err) next(err)
-					else next()
-				})
+	// Get user config, or prompt user to auth their trello account
+	controller.middleware.receive.use((bot, message, next) => {
+		controller.storage.users.get(message,user, (err, user) => {
+			if (! user) {
+				controller.trigger('setupUser', [bot, message])
 			} else {
-				console.log('Have user, looking for channel')
-				controller.storage.channels.get(message.channel, (err, channel) => {
-					if (err) {
-						console.log('Err getting channel from storage: ', err)
-						// controller.trigger('selectBoard', [bot, message])
-					} 
-					message.channel_config = channel 
-					console.log('========CHANNEL CONFIG SET TO: ', channel)
-					// pass in user and channel so our actions are fully configured
-					// create trello api wrapper, requires a trello token
-					// add all our trello functions to the bot object
-
-					bot.trello = controller.trelloActions.create(user, channel)
-					next();
-				})
-
+				message.trello_user = user
+				next()
 			}
+		})
 
-			})
-	 function setupUser(message, cb) {
-	 console.log('======HEARD SETUP USER')
-	 const userObj = {
-		 id: message.user,
-		 token: '',
-		 first_name: '',
-		 last_name: '', 
-		 email: ''
-	 }
+	})
 
-	 controller.storage.users.save(userObj, (err, user) => {
-		 if (err) console.log('Error saving user:', err)
-		 else console.log({user})
+	// Get channel config, or prompt user to set up a board for the channel
+	controller.middleware.receive.user((bot, message, next) => {
+		controller.storage.channels.get(message.channel, (err, channel) => {
+			if (! channel) {
+				controller.trigger('selectBoard')
+			} else {
+				message.trello_channel = channel
+				next()
+			}
+		})
+	})
 
-	 controller.storage.channels.get(message.channel, (err, channel) => {
-		 if (err) {
-			 console.log('Err getting channel from storage: ', err)
-		 }
+	// If user and board are set up, configure trello wrapper for their account
+	controller.middleware.receive.use((bot, message, next) => {
+		bot.trello = controller.trelloActions.create(user, channel)
+		next()
+	})
 
-			 message.channel_config = channel 
-			 // pass in user and channel so our actions are fully configured
-			 // create trello api wrapper, requires a trello token
-			 // add all our trello functions to the bot object
-			 bot.trello = controller.trelloActions.create(user, channel)
-			 cb && cb(null, user)
-		 }
-	 )
-	 })
-
-	 }
-     });
-    
 }
 
 
