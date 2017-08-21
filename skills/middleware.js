3f1cecd0aeaebc6ca6077a3dff4bc7c32b635e98
Ben Brown <ben@xoxco.com>
2017-08-21 12:35:53 -0500

    check if bot is already mid convo before firing other events

--- skills/middleware.js
+++ skills/middleware.js
@@ -2,14 +2,26 @@
 module.exports = (controller) => {
 
 	controller.middleware.receive.use((bot, message, next) => {
+
+		bot.findConversation(message, function(convo) {
+			if (convo) {
+				message.in_convo = true;
+			}
+			next();
+		});
+	});
+
+	controller.middleware.receive.use((bot, message, next) => {
+
 		// ignore our own bots messages
 		if (message.user === controller.identity.emails[0]) {
 					return
-				} 
+				}
 		controller.storage.teams.get('trello', (err, config) => {
 			if (! config) {
 				if (process.env.admin_user === message.user) {
 					controller.trigger('setupTrello', [bot, message])
+					if (message.in_convo)  { return next(); }
 				} else {
 					bot.reply(message, "Sorry, I'm waiting to be setup by the administrator")
 				}
@@ -21,25 +33,30 @@ module.exports = (controller) => {
 
 	// Get user config, or prompt user to auth their trello account
 	controller.middleware.receive.use((bot, message, next) => {
+
 		controller.storage.users.get(message.user, (err, user) => {
 			if (! user) {
 				controller.trigger('setupUser', [bot, message])
+				if (message.in_convo)  { return next(); }
 				return
 			} else {
 				message.trello_user = user
 				next()
 			}
 		})
-		
+
 
 	})
 
 	// Get channel config, or prompt user to set up a board for the channel
 	controller.middleware.receive.use((bot, message, next) => {
+
 		controller.storage.channels.get(message.channel, (err, channel) => {
 			if (! channel) {
 				bot.trello = controller.trelloActions.create(message.user)
 				controller.trigger('setupChannel', [bot, message])
+				if (message.in_convo)  { return next(); }
+
 			} else {
 				message.trello_channel = channel
 				next()
@@ -50,6 +67,7 @@ module.exports = (controller) => {
 	// If user and board are set up, configure trello wrapper for their account
 	controller.middleware.receive.use((bot, message, next) => {
 		console.log('=====Setting up wrapper')
+		console.log('DEALING WITH MESSAGE:', message);
 		bot.trello = controller.trelloActions.create(message.trello_user, message.trello_channel)
 		next()
 	})
@@ -59,7 +77,7 @@ module.exports = (controller) => {
 			id: message.channel
 		}, (err, channel) => {
 			if (err) {
-				console.log(err) 
+				console.log(err)
 			} else {
 				controller.trigger('selectBoard', [bot, message])
 			}
@@ -68,7 +86,3 @@ module.exports = (controller) => {
 
 
 }
-
-
-
-
