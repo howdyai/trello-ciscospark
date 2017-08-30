d054c39443e7a60e730383575eb02396a34e9c3c
jonchurch <me@jonchurch.com>
2017-08-30 19:07:40 -0400

    add basic json config file store

---
+++ components/config.js
@@ -0,0 +1,36 @@
+
+const fs = require('fs')
+
+const model = {
+	orgId: '',//match.orgId,
+	orgName: '',//match.name,
+	token: ''//message.token
+}
+
+exports.get = () => {
+	return new Promise((resolve, reject) => {
+		const json = fs.readFileSync('./config.json')
+
+		try {
+			const data = JSON.parse(json);
+			resolve(data)
+		}
+		catch (err) {
+			reject(err)
+		}
+	})
+}
+
+exports.save = (data) => {
+	return new Promise((resolve, reject) => {
+		const data = JSON.stringify(data);
+
+		fs.writeFile('./config.json', data, (err) => {
+			if (err) {
+				reject(err)
+			} else {
+				resolve(true)
+			}
+		})	
+	})
+}
